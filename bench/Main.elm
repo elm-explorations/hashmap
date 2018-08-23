dee9141472749523e752d8584822fb7618907809
Robin Heggelund Hansen <robin.heggelund@icloud.com>
2018-08-23 18:49:20 +0200

    Use elm-format on benchmark code.

--- bench/Main.elm
+++ bench/Main.elm
@@ -1,7 +1,7 @@
 module Main exposing (main)
 
+import Benchmark exposing (Benchmark, benchmark, describe)
 import Benchmark.Runner exposing (BenchmarkProgram, program)
-import Benchmark exposing (Benchmark, describe, benchmark)
 import Dict.LLRB as Dict
 import Hash.Dict as Dict2
 
@@ -49,63 +49,63 @@ suite n =
         keys =
             List.map (\( k, v ) -> k) ls
     in
-        describe (toString n ++ " elements")
-            [ Benchmark.compare "Get"
-                "LLRB"
-                (\_ -> getter Dict.get keys original)
-                "Hash"
-                (\_ -> getter Dict2.get keys updated)
-            , Benchmark.compare "Insert"
-                "LLRB"
-                (\_ -> Dict.fromList ls)
-                "Hash"
-                (\_ -> Dict2.fromList ls)
-            , Benchmark.compare "Remove"
-                "Core"
-                (\_ -> remover Dict.remove keys original)
-                "Hash"
-                (\_ -> remover Dict2.remove keys updated)
-            , Benchmark.compare "Remove one item"
-                "Core"
-                (\_ -> singleRemover Dict.remove keys original)
-                "Hash"
-                (\_ -> singleRemover Dict2.remove keys updated)
-            , Benchmark.compare "Update insert"
-                "Core"
-                (\_ -> updater Dict.update (\_ -> Just -1) keys original)
-                "Hash"
-                (\_ -> updater Dict2.update (\_ -> Just -1) keys updated)
-            , Benchmark.compare "Update remove"
-                "Core"
-                (\_ -> updater Dict.update (\_ -> Nothing) keys original)
-                "Hash"
-                (\_ -> updater Dict2.update (\_ -> Nothing) keys updated)
-            , Benchmark.compare "Map"
-                "Core"
-                (\_ -> Dict.map (\k v -> v + 1) original)
-                "Hash"
-                (\_ -> Dict2.map (\k v -> v + 1) updated)
-            , Benchmark.compare "Filter"
-                "Core"
-                (\_ -> Dict.filter (\k v -> v % 2 == 0) original)
-                "Hash"
-                (\_ -> Dict2.filter (\k v -> v % 2 == 0) updated)
-            , Benchmark.compare "Union"
-                "Core"
-                (\_ -> Dict.union original originalSetDict)
-                "Hash"
-                (\_ -> Dict2.union updated updatedSetDict)
-            , Benchmark.compare "Intersect"
-                "Core"
-                (\_ -> Dict.intersect original originalSetDict)
-                "Hash"
-                (\_ -> Dict2.intersect updated updatedSetDict)
-            , Benchmark.compare "Diff"
-                "Core"
-                (\_ -> Dict.diff original originalSetDict)
-                "Hash"
-                (\_ -> Dict2.diff updated updatedSetDict)
-            ]
+    describe (toString n ++ " elements")
+        [ Benchmark.compare "Get"
+            "LLRB"
+            (\_ -> getter Dict.get keys original)
+            "Hash"
+            (\_ -> getter Dict2.get keys updated)
+        , Benchmark.compare "Insert"
+            "LLRB"
+            (\_ -> Dict.fromList ls)
+            "Hash"
+            (\_ -> Dict2.fromList ls)
+        , Benchmark.compare "Remove"
+            "Core"
+            (\_ -> remover Dict.remove keys original)
+            "Hash"
+            (\_ -> remover Dict2.remove keys updated)
+        , Benchmark.compare "Remove one item"
+            "Core"
+            (\_ -> singleRemover Dict.remove keys original)
+            "Hash"
+            (\_ -> singleRemover Dict2.remove keys updated)
+        , Benchmark.compare "Update insert"
+            "Core"
+            (\_ -> updater Dict.update (\_ -> Just -1) keys original)
+            "Hash"
+            (\_ -> updater Dict2.update (\_ -> Just -1) keys updated)
+        , Benchmark.compare "Update remove"
+            "Core"
+            (\_ -> updater Dict.update (\_ -> Nothing) keys original)
+            "Hash"
+            (\_ -> updater Dict2.update (\_ -> Nothing) keys updated)
+        , Benchmark.compare "Map"
+            "Core"
+            (\_ -> Dict.map (\k v -> v + 1) original)
+            "Hash"
+            (\_ -> Dict2.map (\k v -> v + 1) updated)
+        , Benchmark.compare "Filter"
+            "Core"
+            (\_ -> Dict.filter (\k v -> v % 2 == 0) original)
+            "Hash"
+            (\_ -> Dict2.filter (\k v -> v % 2 == 0) updated)
+        , Benchmark.compare "Union"
+            "Core"
+            (\_ -> Dict.union original originalSetDict)
+            "Hash"
+            (\_ -> Dict2.union updated updatedSetDict)
+        , Benchmark.compare "Intersect"
+            "Core"
+            (\_ -> Dict.intersect original originalSetDict)
+            "Hash"
+            (\_ -> Dict2.intersect updated updatedSetDict)
+        , Benchmark.compare "Diff"
+            "Core"
+            (\_ -> Dict.diff original originalSetDict)
+            "Hash"
+            (\_ -> Dict2.diff updated updatedSetDict)
+        ]
 
 
 getter : (a -> b -> c) -> List a -> b -> List c
