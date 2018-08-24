6187886977ea9fb5b8b952463514d025d3b700f1
Robin Heggelund Hansen <robin.heggelund@icloud.com>
2018-08-24 15:49:57 +0200

    Improve hashing of objects.

--- src/Hash/FNV.elm
+++ src/Hash/FNV.elm
@@ -3,6 +3,11 @@ module Hash.FNV exposing (hash)
 import Native.FNV
 
 
+fnvOffset : Int
+fnvOffset =
+    2166136261
+
+
 hash : a -> Int
 hash =
-    Native.FNV.hash
+    Native.FNV.hash fnvOffset
--- src/Native/FNV.js
+++ src/Native/FNV.js
@@ -1,20 +1,17 @@
 var _Skinney$elm_hashmap_exploration$Native_FNV = function() {
     var fnvPrime = 16777619;
-    var fnvOffset = 2166136261;
 
-    function hash(object) {
+    function hash(current, object) {
 	switch (typeof object) {
-	case 'string': return hashString(object);
-	case 'number': return hashNum(object);
-	case 'boolean': return object ? hashNum(1) : hashNum(0);
-	case 'object': return hashObj(object);
+	case 'string': return hashString(current, object);
+	case 'number': return hashNum(current, object);
+	case 'boolean': return object ? hashNum(current, 1) : hashNum(current, 0);
+	case 'object': return hashObj(current, object);
 	default: return 0;
 	}
     }
 
-    function hashString(str) {
-	var current = fnvOffset;
-
+    function hashString(current, str) {
 	for (var i = 0, len = str.length; i < len; i++) {
 	    current = (current ^ str.charCodeAt(i)) * fnvPrime;
 	}
@@ -22,22 +19,20 @@ var _Skinney$elm_hashmap_exploration$Native_FNV = function() {
 	return current >>> 0;
     }
 
-    function hashNum(num) {
-	return ((fnvOffset ^ num) * fnvPrime) >>> 0;
+    function hashNum(current, num) {
+	return ((current ^ num) * fnvPrime) >>> 0;
     }
 
-    function hashObj(obj) {
-	var current = fnvOffset;
-
+    function hashObj(current, obj) {
 	for (var key in obj) {
-	    current = (current ^ hash(obj[key])) * fnvPrime;
+	    current = hash(current, obj[key]);
 	}
 
 	return current >>> 0;
     }
 
 return {
-    hash: hash
+    hash: F2(hash)
 };
 
 }();
